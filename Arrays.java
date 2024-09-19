1a77ebc49e850e231bc11bc2fbc975505e8e9c1d
Arnav Thakare <162623054+errorbegone@users.noreply.github.com>
2024-09-19 22:26:07 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args){
+
+		int[] arr = {1, 2, 3}; // static
+		int[] arr = new int[5]; // dynamic
+
+		// length
+		int n = arr.length;
+
+		// sort
+		Arrays.sort(arr);
+
+		// compare
+		boolean compare = Arrays.equals(arr1, arr2)
+
+		// to String
+		String s = Arrays.toString(arr);
+
+	}
+}
